7f8de2998ca1cc5d29c56256044b6c42c95035fe
julien-damilot-vinci <julien.damilot@gmail.com>
2024-11-09 15:04:15 +0100

    feat: add one_minute_line and tests

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -1,2 +1,12 @@
 import { Main } from '../src/app.js';
 
+describe("A suite of test for the function one_minute_line", function() {
+    it("should return '1 lampes allumees'", function() {
+        let main = new Main();
+        expect(main.one_minute_line(1731159131)).toBe('2 lampes allumees');
+        expect(main.one_minute_line(1731160827)).toBe('0 lampes allumees');
+        expect(main.one_minute_line(1731153405)).toBe('1 lampe allumee');
+        expect(main.one_minute_line(1731153525)).toBe('3 lampes allumees');
+        expect(main.one_minute_line(1731153585)).toBe('4 lampes allumees');
+    });
+});
--- src/app.js
+++ src/app.js
@@ -1,3 +1,24 @@
 export class Main {
-  
+    one_minute_line(timestamp) {
+        let minutes = Math.floor(timestamp / 60);
+        console.log(timestamp);
+        console.log(minutes);
+        if (minutes> 0) {
+            if (minutes % 5 === 0) {
+                return '0 lampes allumees';
+            }
+            if (minutes % 5 === 1) {
+                return '1 lampe allumee';
+            }
+            if (minutes % 5 === 2) {
+                return '2 lampes allumees';
+            }
+            if (minutes % 5 === 3) {
+                return '3 lampes allumees';
+            }
+            if (minutes % 5 === 4) {
+                return '4 lampes allumees';
+            }
+        }
+    }
 }
